939863bbd2634f58e6b16ddd9c316fbba9b658ba
Daniel Rooein <Daniel.Rooein@gmail.com>
2020-09-30 21:18:25 -0400

    Add action types as constant

--- src/components/courses/CoursesPage.jsx
+++ src/components/courses/CoursesPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as courseActions from '../../redux/actions/courseActions';
 import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
 
 class CoursesPage extends React.Component {
     state = {
@@ -17,7 +18,7 @@ class CoursesPage extends React.Component {
 
     handleSubmit = event => {
         event.preventDefault();
-        this.props.createCourse(this.state.course);
+        this.props.actions.createCourse(this.state.course);
     }
 
     render() {
@@ -37,7 +38,7 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
     courses: PropTypes.array.isRequired,
-    createCourse: PropTypes.object.isRequired
+    actions: PropTypes.object.isRequired
 }
 
 const mapStateToProps = (state) => {
@@ -46,8 +47,10 @@ const mapStateToProps = (state) => {
     };
 }
 
-const mapDispatchToProps = {
-    createCourse: courseActions.createCourse
+const mapDispatchToProps = dispatch => {
+    return {
+        actions: bindActionCreators(courseActions, dispatch)
+    }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
