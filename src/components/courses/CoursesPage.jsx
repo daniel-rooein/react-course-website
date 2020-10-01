35dd63b2721777f3c9236debdb051387aae984ee
Daniel Rooein <Daniel.Rooein@gmail.com>
2020-09-30 20:29:08 -0400

    Using mapDispatchToProps via bindActionCreators

--- src/components/courses/CoursesPage.jsx
+++ src/components/courses/CoursesPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as courseActions from '../../redux/actions/courseActions';
 import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
 
 class CoursesPage extends React.Component {
     state = {
@@ -17,7 +18,7 @@ class CoursesPage extends React.Component {
 
     handleSubmit = event => {
         event.preventDefault();
-        this.props.createCourse(this.state.courses);
+        this.props.actions.createCourse(this.state.course);
     }
 
     render() {
@@ -37,7 +38,7 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
     courses: PropTypes.array.isRequired,
-    createCourse: PropTypes.func.isRequired
+    actions: PropTypes.object.isRequired
 }
 
 const mapStateToProps = (state) => {
@@ -48,7 +49,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        createCourse: course => dispatch(courseActions.createCourse(course))
+        actions: bindActionCreators(courseActions, dispatch)
     }
 }
 
